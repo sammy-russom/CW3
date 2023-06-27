1a2aeaf6df9df3eb2898c4ca0ccb8c9873de43f6
Sammy Russom <samuelrussom5@gmail.com>
2023-06-27 21:50:54 +0400

    Update App.vue

--- app/src/App.vue
+++ app/src/App.vue
@@ -198,13 +198,7 @@ header button{
     width: 100%;
     object-fit: cover;
 }
-.subject *{
-    margin: 0.25rem 0;
-}
-#Checkout{
-    width: 30%;
-    align-items: center;
-}
+
 
 .input_container{
   margin: 0.5rem 0;
